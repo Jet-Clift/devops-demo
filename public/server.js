25e6dca4a78256575e9eb319a4ba472d14d50ea2
jet-clift <jet.clift@gmail.com>
2023-12-21 10:00:39 -0800

    proyecto

---
+++ public/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.json())
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: 'e95db87eebff46bb97fd050d0f74ffd3',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+
+})
+rollbar.log('hello World!')
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
